34e5f6e7f68c254cec6b96ea689137e65423ace1
Tarek Muhammed <tarek.3d.3dmax@gmail.com>
2019-12-21 18:12:41 +0200

    UPDATE DATABASE_URL

--- social_wall/settings.py
+++ social_wall/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -140,3 +141,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
